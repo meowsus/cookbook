9d3a52ae9fd825fb446e88b2fed99b3e37d6f9ab
meowsus <tooblies@gmail.com>
2025-09-02 18:12:11 -0700

    Add zod validation to API routes

--- src/app/api/sources/[sourceId]/html/route.ts
+++ src/app/api/sources/[sourceId]/html/route.ts
@@ -1,18 +1,33 @@
 import { getSource } from "@/lib/db/sources";
 import { NextResponse } from "next/server";
+import { z } from "zod";
+
+const schema = z.object({
+  sourceId: z.string(),
+});
 
 export async function GET(
   request: Request,
   { params }: { params: Promise<{ sourceId: string }> },
 ) {
-  const { sourceId } = await params;
+  const parsedParams = schema.safeParse(await params);
+
+  if (!parsedParams.success) {
+    return NextResponse.json(
+      { message: "Invalid request parameters" },
+      { status: 400 },
+    );
+  }
+
+  const { sourceId } = parsedParams.data;
+
   const source = await getSource(sourceId);
 
   const url = source?.url;
 
   if (!url) {
     return NextResponse.json(
-      { error: "Source url not found" },
+      { message: "Source url not found" },
       { status: 404 },
     );
   }

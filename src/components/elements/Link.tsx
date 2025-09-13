6b9dfb15f0cd0422f6af74b685602f9f5452113e
meowsus <tooblies@gmail.com>
2025-09-13 13:06:57 -0700

    Remove Link component

--- src/components/elements/Link.tsx
+++
@@ -1,7 +0,0 @@
-import NextLink from "next/link";
-
-export default function Link({
-  ...props
-}: React.ComponentProps<typeof NextLink>) {
-  return <NextLink {...props} className="text-blue-500 hover:text-blue-600" />;
-}

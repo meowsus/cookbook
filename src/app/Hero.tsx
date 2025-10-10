7cce6205d62a8793fcaf244b8b9f6ca2a447abd5
meowsus <tooblies@gmail.com>
2025-10-10 07:32:21 -0700

    Update homepage design

---
+++ src/app/Hero.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+
+interface HeroProps {
+  title: string;
+  description: string;
+  buttonText: string;
+  buttonLink: string;
+  backgroundImage: string;
+}
+
+export default function Hero({
+  title,
+  description,
+  buttonText,
+  buttonLink,
+  backgroundImage,
+}: HeroProps) {
+  return (
+    <div
+      className="hero grow bg-top"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+      }}
+    >
+      <div className="hero-overlay"></div>
+      <div className="hero-content text-neutral-content text-center">
+        <div className="max-w-md text-shadow-sm">
+          <h1 className="mb-5 text-5xl font-bold">{title}</h1>
+          <p className="mb-5 text-lg">{description}</p>
+
+          <Link href={buttonLink} className="btn btn-primary">
+            {buttonText}
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}

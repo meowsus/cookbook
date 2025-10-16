c15b392de00d364c8723ee11f48d083b6ebc7976
meowsus <tooblies@gmail.com>
2025-10-16 07:07:19 -0700

    Update Navbar, make responsive

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,38 +1,58 @@
 import Link from "next/link";
 import { auth } from "@/lib/auth";
 import { signInAction, signOutAction } from "@/lib/actions/auth";
+import { Bars3Icon } from "@heroicons/react/24/solid";
 
 export default async function Navbar() {
   const session = await auth();
 
   return (
     <header className="navbar shadow-sm pr-4 bg-base-100 rounded-lg">
-      <div className="flex-1">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <Bars3Icon className="size-5" />
+          </div>
+
+          <ul
+            tabIndex={-1}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <Link href="/sources">Sources</Link>
+            </li>
+            <li>
+              <Link href="/recipes">Recipes</Link>
+            </li>
+          </ul>
+        </div>
+
         <Link href="/" className="btn btn-ghost text-xl">
           Cookbook
         </Link>
       </div>
 
-      <div className="flex gap-2 items-center">
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link href="/sources">Sources</Link>
+          </li>
+          <li>
+            <Link href="/recipes">Recipes</Link>
+          </li>
+        </ul>
+      </div>
+
+      <div className="navbar-end">
         {session?.user ? (
-          <>
-            <ul className="menu menu-horizontal">
-              <li>
-                <Link href="/">Home</Link>
-              </li>
-              <li>
-                <Link href="/sources">Sources</Link>
-              </li>
-            </ul>
-            <form action={signOutAction}>
-              <button
-                type="submit"
-                className="btn btn-secondary btn-outline btn-sm"
-              >
-                Sign Out
-              </button>
-            </form>
-          </>
+          <form action={signOutAction}>
+            <button
+              type="submit"
+              className="btn btn-secondary btn-outline btn-sm"
+            >
+              Sign Out
+            </button>
+          </form>
         ) : (
           <form action={signInAction}>
             <button

e762438b00224931e1ba622f999d2af988eff949
meowsus <tooblies@gmail.com>
2025-10-17 08:25:40 -0700

    Fix eslint vscode plugin

--- src/app/sources/SourcesList.tsx
+++ src/app/sources/SourcesList.tsx
@@ -12,7 +12,7 @@ export default function SourcesList({ sources }: SourcesListProps) {
   return (
     <ul className="list bg-base-100 rounded-box shadow-md">
       <li className="p-4 pb-2 text-xs opacity-60 tracking-wide">
-        Here are the sources that you've added so far.
+        Here are the sources that you&apos;ve added so far.
       </li>
 
       {sources.map((source) => (

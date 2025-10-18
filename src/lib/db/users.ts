4fdc9ba34606d2992db67278cca143f5b361a1d0
meowsus <tooblies@gmail.com>
2025-10-18 07:57:24 -0700

    Fix tsconfig

--- src/lib/db/users.ts
+++ src/lib/db/users.ts
@@ -1,5 +1,5 @@
+import { Prisma } from "@/generated/prisma";
 import { prisma } from "@/lib/prisma";
-import { Prisma } from "../../../generated/prisma";
 
 export async function createUser(data: Prisma.UserCreateInput) {
   return prisma.user.create({

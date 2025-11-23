4495bfbab2998fb1009b3c516e144fb918f62100
meowsus <tooblies@gmail.com>
2025-11-23 09:25:15 -0800

    Stub out UpdateRecipeDialog component

--- src/app/recipes/[recipeId]/UpdateRecipeForm.tsx
+++ src/app/recipes/[recipeId]/UpdateRecipeForm.tsx
@@ -1,34 +1,52 @@
 "use client";
 
-export default function UpdateRecipeForm({}: { recipeId: string }) {
-  const onSubmit = () => {};
+import UpdateRecipeDialog from "@/app/recipes/[recipeId]/UpdateRecipeDialog";
+import { useRef, useState } from "react";
+
+export default function UpdateRecipeForm({ recipeId }: { recipeId: string }) {
+  const [hasOpened, setHasOpened] = useState(false);
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    dialogRef.current?.showModal();
+    setHasOpened(true);
+  };
 
   return (
-    <form onSubmit={onSubmit} className="space-y-2">
-      <div className="card bg-base-100 shadow-sm">
-        <div className="card-body">
-          <fieldset className="fieldset border-none rounded-box border">
-            <label className="label">Prompt</label>
-            <textarea
-              name="prompt"
-              className="textarea w-full h-64"
-              placeholder="How would you like to modify this recipe?"
-              required
-              defaultValue=""
-            />
-          </fieldset>
+    <>
+      <form onSubmit={onSubmit} className="space-y-2">
+        <div className="card bg-base-100 shadow-sm">
+          <div className="card-body">
+            <fieldset className="fieldset border-none rounded-box border">
+              <label className="label">Prompt</label>
+              <textarea
+                name="prompt"
+                className="textarea w-full h-64"
+                placeholder="How would you like to modify this recipe?"
+                required
+                defaultValue=""
+              />
+            </fieldset>
 
-          <div className="flex gap-2 justify-end">
-            <button type="reset" className="btn btn-ghost">
-              Reset
-            </button>
+            <div className="flex gap-2 justify-end">
+              <button type="reset" className="btn btn-ghost">
+                Reset
+              </button>
 
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-    </form>
+      </form>
+
+      <UpdateRecipeDialog
+        recipeId={recipeId}
+        dialogRef={dialogRef}
+        hasOpened={hasOpened}
+      />
+    </>
   );
 }
